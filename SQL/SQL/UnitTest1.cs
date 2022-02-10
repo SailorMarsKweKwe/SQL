f1a8d6a2b041d4020a38a047a066c829e8d4e6da
innasukhina <innasukhina@gmail.com>
2022-02-10 03:18:23 +0200

    Initial commit

---
+++ SQL/SQL/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using Xunit;
+
+namespace SQL
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            String connectionString = @"Data Source=/Users/innasukhina/Documents/qweqwe.db";
+            List<string>names = new List<string>();
+            List<int> user_ids = new List<int>();
+
+            SQLiteConnection db = new SQLiteConnection(connectionString);
+            db.Open();
+            SQLiteCommand cmd = db.CreateCommand();
+         
+            cmd.CommandText = "SELECT * FROM client";
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                names.Add(reader[0].ToString());
+                user_ids.Add(Convert.ToInt32(reader[1]));
+            }
+            foreach(string a in names)
+            {
+                Console.WriteLine(a);
+            }
+            foreach(int a in user_ids)
+            {
+                Console.WriteLine(a);
+            }
+
+
+        }
+    }
+}
