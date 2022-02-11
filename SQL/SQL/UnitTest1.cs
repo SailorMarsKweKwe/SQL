5b1b37e2d81ba82907b5d2b0d13c7808d934d5ef
innasukhina <innasukhina@gmail.com>
2022-02-11 22:58:20 +0200

    Add

--- SQL/SQL/UnitTest1.cs
+++ SQL/SQL/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace SQL
         public void ArmsByCastleTest()
         {
 
-            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/RiderProjects/SQL_Friday/SQL/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT arms FROM families WHERE castle = (SELECT castle FROM heroes WHERE"+
                 " hero_name = 'Cercei Lannister' AND hero_id = (SELECT hero_id FROM westeros WHERE kingdom = 'Kingdom of the Rock'"+
                 " AND famous_thing = 'Lannister Gold'))", westeros);
@@ -22,7 +22,7 @@ namespace SQL
         public void FamilyIdByHeroNameTest()
         {
 
-            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/RiderProjects/SQL_Friday/SQL/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT family_id FROM westeros WHERE hero_id = (SELECT hero_id FROM" +
                 " heroes WHERE hero_name= 'Robert Baratheon' AND castle='Storms End')", westeros);
             var actual = cmd.ExecuteScalar();
@@ -34,7 +34,7 @@ namespace SQL
         public void HeroKingdomByCastleTest()
         {
 
-            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/RiderProjects/SQL_Friday/SQL/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT kingdom FROM westeros WHERE hero_id = (SELECT hero_id FROM" +
                 " heroes WHERE castle='Highgarden' AND hero_name= 'Loras Tyrell')", westeros);
             var actual = cmd.ExecuteScalar();
@@ -45,7 +45,7 @@ namespace SQL
         public void FirstVictimByHeroIdTest()
         {
 
-            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/RiderProjects/SQL_Friday/SQL/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT first_victim FROM heroes WHERE hero_id=(SELECT hero_id" +
                 " FROM westeros WHERE family_id=(SELECT family_id FROM families WHERE family_name " +
                 "= 'Baratheon' AND castle = 'Storms End') AND kingdom = 'Kingdom of the Stormlands') ", westeros);
@@ -57,7 +57,7 @@ namespace SQL
         public void CastleByFamilyIdTest()
         {
 
-            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/RiderProjects/SQL_Friday/SQL/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT castle FROM families WHERE family_id=(SELECT family_id" +
                 " FROM westeros WHERE famous_thing= 'Dornish wine' AND hero_id=(SELECT hero_id FROM heroes WHERE hero_name"+
                 " = 'Oberyn Martell' AND first_victim = 'knight of Kings Landing ') AND kingdom = 'Principality of Dorne') ", westeros);
