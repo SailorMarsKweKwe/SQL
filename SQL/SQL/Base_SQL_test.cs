ac8c070648e7255d51c53922258921f0390748b4
innasukhina <innasukhina@gmail.com>
2022-02-11 18:09:03 +0200

    Add again

--- SQL/SQL/Base_SQL_test.cs
+++ SQL/SQL/Base_SQL_test.cs
@@ -1,7 +1,37 @@
+using System;
+//using System.Collections.Generic;
+//using System.Data;
+using System.Data.SQLite;
+//using Xunit;
+
 namespace SQL
 {
-    public class Base_SQL_test
+    public class SQL_Helper
     {
-        
+        // Метод что бы открыть связь с БД (типо как делали в BaseTest с браузером или API_Helper). 
+        public static SQLiteConnection OpenConnectionDB (string localPathToDB)
+        {
+            // Создаем строку, по которой код будет коннектиться с БД.
+            String connectionString = localPathToDB;
+            // Создаем связь под названием westeros и открываем эту связь с БД (БД называется westeros).
+            SQLiteConnection westeros = new SQLiteConnection(connectionString);
+            westeros.Open();
+            return westeros;
+        }
+
+        // Метод, который отдает команды Базе Данных.
+        // SqlCommand(String, SqlConnection).Инициализирует новый экземпляр класса SqlCommand текстом запроса и подключением SqlConnection.
+        public static SQLiteCommand ManageDB(string manage, SQLiteConnection westeros)
+        {
+            SQLiteCommand cmd = westeros.CreateCommand();
+            cmd.CommandText = manage;
+            return cmd;
+        }
+
+        // Метод, который прерывает связь с БД.
+        public static void CloseConnectionDB(SQLiteConnection westeros)
+        {
+            westeros.Close();
+        }
     }
 }
