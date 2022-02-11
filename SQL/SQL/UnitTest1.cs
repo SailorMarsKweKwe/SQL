7e6ef64f624d7a80e537cad1658b9e3ac3b408f1
innasukhina <innasukhina@gmail.com>
2022-02-11 20:23:44 +0200

    Add a little bit harder test just for practice

--- SQL/SQL/UnitTest1.cs
+++ SQL/SQL/UnitTest1.cs
@@ -18,6 +18,16 @@ namespace SQL
             Assert.Equal("Arryn", actual);
             SQL_Helper.CloseConnectionDB(westeros);
         }
+        [Fact]
+        public void LittleBitHarderTest()
+        {
+
+            SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
+            SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT family_id FROM westeros WHERE hero_id = (SELECT hero_id FROM heroes WHERE hero_name= 'Robert Baratheon')", westeros);
+            var actual = cmd.ExecuteScalar();
+            Assert.Equal(7, Convert.ToInt64(actual));
+            SQL_Helper.CloseConnectionDB(westeros);
+        }
         /*
         [Fact]
         public void Test1()
