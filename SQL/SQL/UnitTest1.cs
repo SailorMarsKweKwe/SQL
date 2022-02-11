4299517130071f7b8c9fd017d4109c50d6e63481
innasukhina <innasukhina@gmail.com>
2022-02-11 22:14:40 +0200

    Add another one test

--- SQL/SQL/UnitTest1.cs
+++ SQL/SQL/UnitTest1.cs
@@ -18,14 +18,13 @@ namespace SQL
             Assert.Equal("Arryn", actual);
             SQL_Helper.CloseConnectionDB(westeros);
         }
-        
         [Fact]
         public void FamilyIdByHeroNameTest()
         {
 
             SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT family_id FROM westeros WHERE hero_id = (SELECT hero_id FROM" +
-                                                    " heroes WHERE hero_name= 'Robert Baratheon' AND castle='Storms End')", westeros);
+                " heroes WHERE hero_name= 'Robert Baratheon' AND castle='Storms End')", westeros);
             var actual = cmd.ExecuteScalar();
             Assert.Equal(7, Convert.ToInt64(actual));
             SQL_Helper.CloseConnectionDB(westeros);
@@ -37,33 +36,31 @@ namespace SQL
 
             SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT kingdom FROM westeros WHERE hero_id = (SELECT hero_id FROM" +
-                                                    " heroes WHERE castle='Highgarden' AND hero_name= 'Loras Tyrell')", westeros);
+                " heroes WHERE castle='Highgarden' AND hero_name= 'Loras Tyrell')", westeros);
             var actual = cmd.ExecuteScalar();
             Assert.Equal("Kingdom of the Reach", actual);
             SQL_Helper.CloseConnectionDB(westeros);
         }
-        
         [Fact]
         public void FirstVictimByHeroIdTest()
         {
 
             SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT first_victim FROM heroes WHERE hero_id=(SELECT hero_id" +
-                                                    " FROM westeros WHERE family_id=(SELECT family_id FROM families WHERE family_name " +
-                                                    "= 'Baratheon' AND castle = 'Storms End') AND kingdom = 'Kingdom of the Stormlands') ", westeros);
+                " FROM westeros WHERE family_id=(SELECT family_id FROM families WHERE family_name " +
+                "= 'Baratheon' AND castle = 'Storms End') AND kingdom = 'Kingdom of the Stormlands') ", westeros);
             var actual = cmd.ExecuteScalar();
             Assert.Equal("Rhaegar Targaryen", actual);
             SQL_Helper.CloseConnectionDB(westeros);
         }
-        
         [Fact]
         public void CastleByFamilyIdTest()
         {
 
             SQLiteConnection westeros = SQL_Helper.OpenConnectionDB(@"Data Source =/Users/innasukhina/Documents/westeros.db");
             SQLiteCommand cmd = SQL_Helper.ManageDB("SELECT castle FROM families WHERE family_id=(SELECT family_id" +
-                                                    " FROM westeros WHERE famous_thing= 'Dornish wine' AND hero_id=(SELECT hero_id FROM heroes WHERE hero_name"+
-                                                    " = 'Oberyn Martell' AND first_victim = 'knight of Kings Landing ') AND kingdom = 'Principality of Dorne') ", westeros);
+                " FROM westeros WHERE famous_thing= 'Dornish wine' AND hero_id=(SELECT hero_id FROM heroes WHERE hero_name"+
+                " = 'Oberyn Martell' AND first_victim = 'knight of Kings Landing ') AND kingdom = 'Principality of Dorne') ", westeros);
             var actual = cmd.ExecuteScalar();
             Assert.Equal("Sunspear", actual);
             SQL_Helper.CloseConnectionDB(westeros);
